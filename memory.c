a059cc2bdb029fde97031d2238a6e40e3fb6cb17
Trishala-K1 <121046665+Trishala-K1@users.noreply.github.com>
2023-07-27 21:42:04 -0400

    Update memory.c

--- memory.c
+++ memory.c
@@ -23,29 +23,40 @@ struct MEMORY_BLOCK set_null(struct MEMORY_BLOCK *block)
 //........................................................................................................................................
 
 //best_fit_allocate:
-struct MEMORY_BLOCK best_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX], int *map_cnt, int process_id) {
-    int best_fit_idx = -1;
-    int best_fit_size = INT_MAX;
-
-    for (int i = 0; i < *map_cnt; i++)
-    {
-        struct MEMORY_BLOCK block = memory_map[i];
-        if (block.process_id == FREE) {
-            if (block.segment_size == request_size)
-            {
-                block.process_id = process_id;
-                memory_map[i] = block;
-                return block;
-            } else if (block.segment_size > request_size && block.segment_size < best_fit_size) {
-                best_fit_size = block.segment_size;
-                best_fit_idx = i;
-            }
-        }
-    }
-
-    if (best_fit_idx == -1 || *map_cnt + 1 > MAPMAX) {
-        return NULLBLOCK;
-    }
+struct MEMORY_BLOCK best_fit_allocate(int request_size, struct MEMORY_BLOCK memory_map[MAPMAX],int *map_cnt, int process_id){
+	int sizeDiff = 10000;
+	int index = 0;
+	for (int i = 0; i < *map_cnt; i++){
+		if(memory_map[i].segment_size - request_size <= sizeDiff && memory_map[i].process_id == 0){
+			sizeDiff = memory_map[i].segment_size - request_size;
+			index = i;
+		}
+	}
+	if(sizeDiff == 10000){
+		struct MEMORY_BLOCK nullblock;
+		return set_null(&nullblock);
+	}
+	else if(sizeDiff == 0){
+		memory_map[index].process_id = process_id;
+		return memory_map[index];
+	}
+	else{
+		struct MEMORY_BLOCK chunk;
+		chunk.process_id = 0;
+		chunk.start_address = memory_map[index].start_address + request_size;
+		chunk.end_address = memory_map[index].end_address;
+		chunk.segment_size = chunk.end_address - chunk.start_address + 1;
+		memory_map[*map_cnt] = chunk;
+		(*map_cnt)++;
+		
+		memory_map[index].end_address = memory_map[index].start_address + request_size - 1;
+		memory_map[index].process_id = process_id;
+		memory_map[index].segment_size = request_size;
+		
+		return memory_map[index];
+		
+	}
+}
 
 //........................................................................................................................................
 
